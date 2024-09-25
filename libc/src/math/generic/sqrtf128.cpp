78bc9af41a1fdad1c937f2cb8795dae27907cbcb
Alexei Sibidanov <sibid@uvic.ca>
2024-09-25 16:55:42 -0700

    [as_sqrtf128]: first revision, uses _mm_rsqrt_ss instruction as initial approximation for the reciprocal square root, then it is refined up to 60 bits using the higher order Newton iteration. Then the reciprocal is multiplied by the argument with full 128 bit precision to get the initial approximation of the square root itself which then again is refined by the second order Newton iteration. The correct rounding is provided by squareing the result and comparing it with the argument. It would be nice to explore other ways for the initial reciprocal approximation in future. In test sample the hard to round cases for the round to nearest moded are used as well as exact cases which are the hard to round cases for the directional modes. Currently errno is not set only floatint point exceptions are raising.

--- libc/src/math/generic/sqrtf128.cpp
+++ libc/src/math/generic/sqrtf128.cpp
@@ -1,5 +1,7 @@
 //===-- Implementation of sqrtf128 function -------------------------------===//
 //
+// Copyright (c) 2024 Alexei Sibidanov <sibid@uvic.ca>
+//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
@@ -7,14 +9,231 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/math/sqrtf128.h"
-#include "src/__support/FPUtil/sqrt.h"
 #include "src/__support/common.h"
-#include "src/__support/macros/config.h"
+#include "src/__support/CPP/bit.h"
+#include "src/__support/uint128.h"
+#include "src/__support/macros/properties/cpu_features.h"
+
+#include <stdio.h>
+#include <fenv.h>
+#include <x86intrin.h>
+
+#define likely(x)      __builtin_expect(!!(x), 1)
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+#define __as_isnan(x) ((x.b[1]&(~0ul>>16))|x.b[0])
+#define __as_issnan(x) (!(x.b[1]&(1l<<47)))
+#define __as_snan2qnan(x) (x.b[1]|=(1l<<47))
 
 namespace LIBC_NAMESPACE_DECL {
+  typedef UInt128 u128;
+  typedef Int128 i128;
+  typedef uint64_t u64;
+  typedef int64_t i64;
+  typedef uint32_t u32;
+  typedef int32_t i32;
 
-LLVM_LIBC_FUNCTION(float128, sqrtf128, (float128 x)) {
-  return fputil::sqrt<float128>(x);
-}
+  union int128_64_32{
+    u128 a;
+    i128 as;
+    u64 b[2];
+    i64 bs[2];
+    u32 c[4];
+    i32 cs[4];
+    float128 f;
+    __m128i x;
+  };
 
+  float128 reinterpret_u128_as_f128(u128 x){
+#ifdef LIBC_TARGET_CPU_HAS_SSE4_2
+    u64 x0 = (u64)x, x1 = (u64)(x>>64);
+    __m128i m = {0, 0};
+    m = _mm_insert_epi64 (m, x0, 0);
+    m = _mm_insert_epi64 (m, x1, 1);
+    return cpp::bit_cast<float128>(m);
+#else
+    return cpp::bit_cast<float128>(x);
+#endif
+  }
+
+  static inline __m128i f128_as_m128i(float128 z){
+    return cpp::bit_cast<__m128i>(z);
+  }
+
+  static inline u64 mhuu(u64 _a, u64 _b){
+    return ((u128)_a*_b)>>64;
+  }
+
+  static inline i64 mhii(i64 x, i64 y){
+    return ((i128)x*y)>>64;
+  }
+
+  static inline u128 mhuU(u64 y, u128 x){
+    union int128_64_32 ux; ux.a = x;
+    u128 xy0 = ux.b[0]*(u128)y;
+    u128 xy1 = ux.b[1]*(u128)y;
+    return xy1 + (xy0>>64);
+  }
+
+  static inline u128 mhUU(u128 _a, u128 _b){
+    int128_64_32 a, b, a1b0, a0b1, a1b1;
+    a.a = _a;
+    b.a = _b;
+    a1b0.a = (u128)a.b[1]*b.b[0];
+    a0b1.a = (u128)a.b[0]*b.b[1];
+    a1b1.a = (u128)a.b[1]*b.b[1];
+    a1b1.a += a1b0.b[1];
+    a1b1.a += a0b1.b[1];
+    return a1b1.a;
+  }
+
+  static inline i128 mhUIm(u128 _a, i128 _b, u64 mask){
+    union int128_64_32 sub; sub.a = _a;
+    sub.b[0] &= mask;
+    sub.b[1] &= mask;
+    return mhUU(_a,_b) - sub.a;
+  }
+
+  static inline i128 mhIU(i128 _b, u128 _a){
+    return mhUIm(_a,_b,(u64)(_b>>127));
+  }
+
+  LLVM_LIBC_FUNCTION(float128, sqrtf128, (float128 x)) {
+    u32 flagp = _mm_getcsr(), oflagp = flagp;
+    __m128i m = f128_as_m128i(x);
+    int128_64_32 u;
+    u.b[1] = m[1];
+    u.b[0] = m[0];
+    u32 e = u.b[1]>>48;
+    i32 q2 = (e+1)>>1, i = (e+1)&1;
+    i64 e2 = (q2+8191ul-1)<<48;
+    if (unlikely((e-1)>=0x7ffe)){// all special cases NAN, inf, subnormals and negative numbers
+      i32 sign = e>>15;
+      e &= 0x7fff;
+      if(unlikely(e==0x7fff)){// NAN or inifinity
+	if(__as_isnan(u)){ // NAN
+	  if(__as_issnan(u)){ // signaling nan
+	    flagp |= FE_INVALID;
+	    __as_snan2qnan(u); // transform snan to qnan
+	  }
+	  x = reinterpret_u128_as_f128(u.a);
+	} else { //infinity
+	  if (sign){
+	    flagp |= FE_INVALID;
+	    x = __builtin_nanf128("negsqrt");
+	  }
+	}
+	if(flagp != oflagp) _mm_setcsr(flagp);
+	return x;
+      }
+      if (unlikely((u.a<<1)==0)) return x; // signed zero
+      if (unlikely(sign)){ // negative number
+	_mm_setcsr(flagp|FE_INVALID);
+	return __builtin_nanf128("negsqrt");
+      }
+      // denormal
+      i32 ns = -15;
+      if (u.b[1]){
+	ns += __builtin_clzll(u.b[1]);
+      } else {
+	ns += __builtin_clzll(u.b[0]) + 64;
+      }
+      u.a <<= ns;
+      e -= ns-122;
+      u.b[1] ^= (u64)(ns&1)<<48;
+      m = cpp::bit_cast<__m128i>(reinterpret_u128_as_f128(u.a));
+      i = e&1;
+      q2 = e>>1;
+      e2 = (q2+8191ul-1-60)<<48;
+    }
+
+    const __m128i msk = {~0ull>>40, 0}, off = {0x81ll<<23, 0}, sexp = {0x1ffll<<23,0};
+    m = _mm_srli_si128(m, 11);
+    m = _mm_srli_epi64(m, 1);
+    m = _mm_and_si128(m, msk);
+    m = _mm_sub_epi32(m, off);
+    m = _mm_xor_si128(m, sexp);
+    __m128 mf = cpp::bit_cast<__m128>(m);
+    //    printf("%016lx %016lx %a\n",u.b[1],u.b[0],mf[0]);
+    mf = _mm_rsqrt_ss(mf); // get first approximation of reciprocal square root
+    u.a <<= 16;
+    m = cpp::bit_cast<__m128i>(mf);
+    u32 r = (u32)m[0];
+    r = (r<<8)|1ul<<31;
+    u32 shft = 2 - i;
+    u.a >>= shft;
+    u.b[1] |= 1ul<<(62+i);
+    //    printf("%016lx %016lx %a\n",u.b[1],u.b[0],mf[0]);
+
+    u64 R = r, r2 = (u64)r*r;
+    i64 h = mhuu(u.b[1],r2)<<2; // approximation correction
+    //    printf("%016lx\n",h);
+    u64 h2 = mhii(h,h);
+    i64 h3 = (u128)h2*h>>64;
+    u64 h4 = (h2>>16)*(h2>>16);
+    h -= (h2*3>>2) - (h3*5>>3) + (35*h4>>38); // refine correction
+
+    R<<=31;
+    u64 dR = mhii(h,R);
+    R <<= 1;
+    R -= dR;
+    //    printf("%016lx\n",R);
+
+    u128 sx = mhuU(R, u.a);
+    i128 H  = mhuU(R, sx)<<2;
+    //    printf("%016lx %016lx\n",(i64)(H>>64),(i64)H);
+    i64 hh = (i64)(H>>(14+32+2)), hh2 = 3*(hh*hh);
+    H -= hh2>>(38-4);
+    i128 ds = mhIU(H, sx);
+    sx <<= 1;
+    int128_64_32 v; v.a = sx - ds;
+    //    printf("%016lx %016lx\n",v.b[1],v.b[0]);
+
+    u32 rm = flagp&_MM_ROUND_MASK, nrst = rm == _MM_ROUND_NEAREST;
+    shft = 49 + nrst;
+    i64 dd = (v.bs[0]<<shft)>>shft;
+
+    if(unlikely(!(dd<-2||dd>5))){ // can round correctly?
+      u128 m = v.a>>15, m2 = m*m;
+      int128_64_32 t0, t1;
+      t0.a = m2 - (u.a<<98);
+      if(unlikely(t0.a==0)){
+	v.a = m<<15;
+      } else {
+	t1 = t0;
+	i64 sgn = t0.bs[1]>>63;
+	t1.a -= (m<<1)^sgn;
+	t1.a += 1 + sgn;
+	if(unlikely(t1.a==0)){
+	  v.a = (m - (2*sgn + 1))<<15;
+	} else {
+	  t1.a += t0.a;
+	  i64 side = t1.bs[1]>>63;
+	  v.b[0] &= ~0ul<<15;
+	  v.a -= ((sgn&side)|(~sgn&1l))<<(15+side);
+	  v.a |= 1; // sticky bit
+	}
+      }
+    }
+
+    u64 frac = v.b[0]&0x7ffful; // fractional part
+    u64 rnd; // round bit
+    if(likely(nrst)){
+      rnd = frac>>14;  // round to nearest tie to even
+    } else if(rm == _MM_ROUND_UP){
+      rnd = !!frac; // round up
+    } else if(rm == _MM_ROUND_DOWN){
+      rnd = 0; // round down
+    } else {
+      rnd = 0; // round to zero
+    }
+    v.a >>= 15; // position mantissa
+    v.a += rnd; // round
+
+    // set inexact flag only if square root is really inexact or not already set
+    if(likely(frac)) flagp |= FE_INEXACT;
+    if(unlikely(oflagp != flagp)) _mm_setcsr(flagp);
+
+    v.b[1] += e2; // place exponent
+    return reinterpret_u128_as_f128(v.a); // put into xmm register
+  }
 } // namespace LIBC_NAMESPACE_DECL
--- libc/test/src/math/smoke/sqrtf128_test.cpp
+++ libc/test/src/math/smoke/sqrtf128_test.cpp
@@ -1,5 +1,7 @@
 //===-- Unittests for sqrtf128---------------------------------------------===//
 //
+// Copyright (c) 2024 Alexei Sibidanov <sibid@uvic.ca>
+//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
@@ -11,3 +13,86 @@
 #include "src/math/sqrtf128.h"
 
 LIST_SQRT_TESTS(float128, LIBC_NAMESPACE::sqrtf128)
+
+TEST_F(LlvmLibcSqrtTest, HardToRound) {
+  // Since there is no exact half cases for square root I encode the
+  // round direction in the sign of the result. E.g. if the number is
+  // negative it means that the exact root is below the rounded value
+  // (the absolute value). Thus I can test not only hard to round
+  // cases for the round to nearest mode but also the directional
+  // modes.
+  struct {float128 x, y;} ts[] = {
+    {0x0.000000dee2f5b6a26c8f07f05442p-16382q, -0x1.ddbd8763a617cff753e2a31083p-8204q},
+    {0x0.000000c86d174c5ad8ae54a548e7p-16382q, 0x1.c507bb538940719890851ec1ca88p-8204q},
+    {0x0.000020ab15cfe0b8e488e128f535p-16382q, -0x1.6dccb402560213bc0d62d62e910bp-8201q},
+    {0x0.0000219e97732a9970f2511989bap-16382q, 0x1.73163d28be706f4b5052791e28a5p-8201q},
+    {0x0.000026e477546ae99ef57066f9fdp-16382q, -0x1.8f20dd0d0c570a23ea59bc2bf009p-8201q},
+    {0x0.00002d0f88d27a496b3e533f5067p-16382q, 0x1.ad9d4abe9f047225a7352bcc52c1p-8201q},
+    {0x1.0000000000000000000000000001p+0q, 0x1p+0q},
+    {0x1.0000000000000000000000000003p+0q, 0x1.0000000000000000000000000001p+0q},
+    {0x1.0000000000000000000000000005p+0q, 0x1.0000000000000000000000000002p+0q},
+    {0x1.2af17a4ae6f93d11310c49c11b59p+0q, -0x1.14a3bdf0ea5231f12d421a5dbe33p+0q},
+    {0x1.c4f5074269525063a26051a0ad27p+0q, 0x1.54864e9b1daa4d9135ff00663366p+0q},
+    {0x1.035cb5f298a801dc4be9b1f8cd97p+1q, -0x1.6c688775bffcb3f507ba11d0abb9p+0q},
+    {0x1.274be02380427e709beab4dedeb4p+1q, -0x1.84d5763281f2318422392e506b1cp+0q},
+    {0x1.64e797cfdbaa3f7e2f33279dbc6p+1q, 0x1.ab79b164e255b26eca00ff99cc99p+0q},
+    {0x1.d78d8352b48608b510bfd5c75315p+1q, -0x1.eb5c420f15adce0ed2bde5a241cep+0q},
+    {0x1.fffffffffffffffffffffffffffbp+1q, 0x1.fffffffffffffffffffffffffffdp+0q},
+    {0x1.fffffffffffffffffffffffffffdp+1q, 0x1.fffffffffffffffffffffffffffep+0q},
+    {0x1.ffffffffffffffffffffffffffffp+1q, 0x1.ffffffffffffffffffffffffffffp+0q},
+  };
+
+  auto rnd = [](float128 x, int rm){
+    union {float128 f; unsigned __int128 u;} y;
+    int d = x<0;
+    if(d)
+      y.f = -x;
+    else
+      y.f = x;
+    if(rm==0){//nearest
+    } else if(rm==1){ // up
+      if(!d) y.u += 1;
+    } else { // down or zero
+      if(d) y.u -= 1;
+    }
+    return y.f;
+  };
+
+  for(auto &t: ts){
+    EXPECT_FP_EQ_ROUNDING_NEAREST     (rnd(t.y,0), LIBC_NAMESPACE::sqrtf128(t.x));
+    EXPECT_FP_EQ_ROUNDING_UPWARD      (rnd(t.y,1), LIBC_NAMESPACE::sqrtf128(t.x));
+    EXPECT_FP_EQ_ROUNDING_DOWNWARD    (rnd(t.y,2), LIBC_NAMESPACE::sqrtf128(t.x));
+    EXPECT_FP_EQ_ROUNDING_TOWARD_ZERO (rnd(t.y,3), LIBC_NAMESPACE::sqrtf128(t.x));
+  }
+
+  auto checkexact = [this](float128 x, float128 y){
+    EXPECT_FP_EQ_ROUNDING_NEAREST     (y, LIBC_NAMESPACE::sqrtf128(x));
+    EXPECT_FP_EQ_ROUNDING_UPWARD      (y, LIBC_NAMESPACE::sqrtf128(x));
+    EXPECT_FP_EQ_ROUNDING_DOWNWARD    (y, LIBC_NAMESPACE::sqrtf128(x));
+    EXPECT_FP_EQ_ROUNDING_TOWARD_ZERO (y, LIBC_NAMESPACE::sqrtf128(x));
+  };
+
+  auto checkexactl = [checkexact](unsigned long k){
+    __int128 kx = (__int128)k*k;
+    float128 x = kx, y = k;
+    return checkexact(x,y);
+  };
+
+  // exact results for subnormal arguments
+  struct {float128 x, y;} te[] = {
+    {0x0.0000000000000000000000000001p-16382q, 0x1p-8247q},
+    {0x0.0000000000000000000000000004p-16382q, 0x1p-8246q},
+    {0x0.0000000000001000000000000000p-16382q, 0x1p-8217q},
+    {0x0.0000000000010000000000000000p-16382q, 0x1p-8215q},
+    {0x0.0000000000100000000000000000p-16382q, 0x1p-8213q},
+  };
+
+  for(auto &t: te) checkexact(t.x,t.y);
+
+  // check exact cases starting from small numbers
+  for(unsigned long k=1;k<1000l*1000l;k++) checkexactl(k);
+
+  // then from the largest number
+  unsigned long k0 = 101904826760412362l;
+  for(unsigned long k=k0;k>k0-1000l*1000l;k--) checkexactl(k);
+}
